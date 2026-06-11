8a59ccef9792afaf3ce0e64c9c10b00f6c478726
Htoo Aunt Shein <htooauntshein@Htoos-MacBook-Pro.local>
2026-06-11 22:46:33 +0700

    Enpcasulation

---
+++ src/enpcasulation1/Son.java
@@ -0,0 +1,21 @@
+package enpcasulation1;
+
+public class Son extends Dad{
+	
+	
+	
+	public static void main(String[] args) {
+	  // Same Package Different Class 
+		
+			Son son = new Son();
+			
+			son.phone = "Iphone 15 Pro Max";
+			son.watch =  "G-shock"; 
+			son.cloth = "H&M";
+			
+			son.getPhone();
+			son.getWatch();
+			son.getCloth();
+	}
+
+}
